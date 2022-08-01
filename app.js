6b8ff29f2d096e6ce60dcd6ceee37f593f91e987
EvgeniyWebDeveloper <69044076+EvgeniyWebDeveloper@users.noreply.github.com>
2022-08-01 16:16:46 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const barValues=document.querySelectorAll('.graph_item-value')
 const bars=document.querySelectorAll('.graph__bar')
 const labels=document.querySelectorAll('.graph__label')
 
-async function getData(url='/data.json'){
+async function getData(url='http://EvgeniyWebDeveloper.github.io/mydata.json'){
     const response = await fetch(url)
     const data =await response.json()
     return data
